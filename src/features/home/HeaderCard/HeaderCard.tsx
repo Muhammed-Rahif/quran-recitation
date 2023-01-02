21618333dfcd2f8711a3ff94cafc9a9d6b3a1c82
Muhammed Rahif <rahifpalliyalil@gmail.com>
2023-01-02 18:05:42 +0530

    chore(helpers): add api helpers file

---
+++ src/features/home/HeaderCard/HeaderCard.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  Box,
+  Card,
+  CardBody,
+  Divider,
+  Flex,
+  Heading,
+  Image,
+  LinkBox,
+  LinkOverlay,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+function HeaderCard() {
+  return (
+    <LinkBox>
+      <Card pos="relative" bg="green.700" my={4} borderRadius="2xl">
+        <CardBody pos="relative" zIndex={1}>
+          <Text mb={2} fontSize="xs">
+            Last Read
+          </Text>
+
+          <Box my={3}>
+            <Flex align="center">
+              <Heading>
+                <LinkOverlay as={Link} to="/">
+                  Al-Fatihah
+                </LinkOverlay>
+              </Heading>
+              <Spacer />
+              <Text fontSize="xs">Makkah</Text>
+            </Flex>
+
+            <Text>The Opener</Text>
+            <Divider my={1.5} />
+          </Box>
+
+          <Text fontSize="sm">Ayah No: 7</Text>
+        </CardBody>
+
+        <Image
+          pos="absolute"
+          top="60%"
+          right={4}
+          w={32}
+          transform="translateY(-50%)"
+          src="/images/quran.png"
+          alt=""
+          zIndex={0}
+          opacity={0.25}
+        />
+      </Card>
+    </LinkBox>
+  );
+}
+
+export default HeaderCard;
